ea1894807ff1fcb44c3673e9a21cda101ce4e393
Angelo Alcantara <angeloalcantara4@gmail.com>
2021-11-02 22:14:17 +1300

    Rename dynamodb_helpers folder to datahandler

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client, MessageEmbed } = require('discord.js');
 const { ScanCommand } = require("@aws-sdk/client-dynamodb");
 const { botIntents, commands, prefix } = require('./config/config.js');
 const config = require('./config/default.js');
-const ddbClient = require('./dynamodb_helpers/ddbClient.js')
+const ddbClient = require('./datahandler/ddbClient.js')
 
 // Set the parameters.
 const params = {
@@ -43,47 +43,23 @@ client.on('ready', () => {
 
 client.login(config.DISCORD_TOKEN);
 
-const getLastMsgs = async (msg) => {
-    // fetching the last 10 messages
-    const res = await msg.channel.messages.fetch({ limit: 10 });
-  
-    const lastTenMsgs = res.map((message) => {
-        return message.content;
-      });
-    
-    const embeds = [];
-
-    lastTenMsgs.forEach((msg, index) => {
-        const embed = new MessageEmbed()
-            .setColor('ORANGE') // can be hex like #3caf50
-            .setTitle(`Message ${index + 1}`)
-            .setDescription(`${msg}`)
-            .setFooter('Buddy says Hi');
-        
-        embeds.push(embed);
-    });
-
-    return embeds;
-};
-
 client.on('messageCreate', async (msg) => {
-    console.log(msg);
-    if (msg.author.bot) return;
+    if (msg.author.bot) return; 
     if (!msg.content.startsWith(prefix)) return; // do nothing if command is not preceded with prefix
   
     const userCmd = msg.content.slice(prefix.length);
   
-    if (userCmd === commands.getName) {
-        msg.reply('fugg u ' + msg.author.username);
-    } else if (userCmd === commands.tellJoke) {
-        msg.channel.send('HTML bla bla bla');
-    } else if (userCmd === commands.sad) {
-        msg.reply("Don't be sad! This is not the end of the road");
-    } else if (userCmd === commands.lastMsgs) {
-        const reply = await getLastMsgs(msg);
-        msg.channel.send({ embeds: reply });
-    } else {
-        msg.reply('I do not understand your command');
-    }
+    // if (userCmd === commands.getName) {
+    //     msg.reply('fugg u ' + msg.author.username);
+    // } else if (userCmd === commands.tellJoke) {
+    //     msg.channel.send('HTML bla bla bla');
+    // } else if (userCmd === commands.sad) {
+    //     msg.reply("Don't be sad! This is not the end of the road");
+    // } else if (userCmd === commands.lastMsgs) {
+    //     const reply = await getLastMsgs(msg);
+    //     msg.channel.send({ embeds: reply });
+    // } else {
+    //     msg.reply('I do not understand your command');
+    // }
 });
 
