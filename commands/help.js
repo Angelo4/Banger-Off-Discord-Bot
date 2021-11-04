da810f41ad82e7be41c17fb40f10740b1aae34aa
Angelo Alcantara <angeloalcantara4@gmail.com>
2021-11-04 16:48:43 +1300

    Populate command files with skeleton obj

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: '',
+    desription: '',
+    aliases: '',
+    usage: '', //expected argument usage
+    args: false,
+    execute(message) {
+        //Write command execution logic here
+    },
+};
