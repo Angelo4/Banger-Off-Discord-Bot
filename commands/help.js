73f562905545508bf37f7aae9d146bf258a61b52
Nikhil Prasad <nikhilprasad44@gmail.com>
2021-11-30 23:34:33 +1300

    WIP help command

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,22 @@
 module.exports = {
-    name: '',
-    description: '',
+    name: 'help',
+    description: 'Returns information regarding bot commands.',
     aliases: [],
     usage: '', //expected argument usage
     args: false,
-    execute(message) {
-        //Write command execution logic here
+    execute(message, args, main) {
+        var messageContents = 'Welcome to the greatest bot that has ever graced any Discord server.\n'
+            + 'Hope y\'all are ready to bang.'
+
+        var helpMessage = new main.MessageEmbed()
+            .setDescription(messageContents)
+            .setFooter('Sweet as, lesgeddit!');
+
+        message.reply({embeds: [helpMessage]})
+            .catch((err) => {
+                //Look into error logging in the future
+                console.log(err);
+                message.reply("Sorry something went wrong while asking for help. Please try again later.");
+            })
     },
 };
